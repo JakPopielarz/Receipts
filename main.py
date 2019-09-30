5718e9436109dfe5fb73859e712eea47824ca1cb
JakPopielarz <popielarzjakub7@gmail.com>
2019-09-30 17:57:07 +0200

    Image is scrollable, can reload the image multiple times

--- main.py
+++ main.py
@@ -4,12 +4,57 @@ from tkinter import filedialog
 import tkinter as tk
 from PIL import Image, ImageTk
 
-window = tk.Tk()
+class Window():
+    def __init__(self):
+        self.root = tk.Tk()
+        
+        # set up for the photo - create a scrollable canvas
+        self.photo_frame = tk.Frame(self.root)
+        self.photo_frame.grid()
+        
+        self.canvas = tk.Canvas(self.photo_frame, width=500, height=500)
 
-imagefile = filedialog.askopenfilename(title = "Select photo of a receipt",
-                                       filetypes = (("jpeg files","*.jpg"),
-                                                    ("all files","*.*")))
-img = ImageTk.PhotoImage(Image.open(imagefile))
-lbl = tk.Label(window, image = img).pack()
+        # create and set horizontal scrollbar for the photo
+        self.scroll_x = tk.Scrollbar(self.photo_frame, orient="horizontal",
+                                     command=self.canvas.xview)
+        self.scroll_x.pack(side="bottom", fill="x")
 
-window.mainloop()
+        # create and set vertical scrollbar for the photo
+        self.scroll_y = tk.Scrollbar(self.photo_frame, orient="vertical",
+                                     command=self.canvas.yview)
+        self.scroll_y.pack(side="right", fill="y")
+        
+        self.canvas.configure(yscrollcommand=self.scroll_y.set,
+                              xscrollcommand=self.scroll_x.set)
+        self.canvas.pack(side="left", fill="both", expand=True)
+
+
+        self.load_photo()
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+
+        again_button = tk.Button(self.root, text="Load photo",
+                                 command=self.load_photo)
+        again_button.grid()
+#        again_button.pack()
+
+        self.root.mainloop()
+
+    def load_photo(self):
+        try:
+            image_path = filedialog.askopenfilename(title="Select photo of a receipt",
+                                                    filetypes=(("jpeg files", "*.jpg"),
+                                                               ("all files", "*.*")))
+            self.image = ImageTk.PhotoImage(Image.open(image_path))
+            self.canvas.create_image(0, 0, anchor="nw", image=self.image)
+        except OSError:
+            self.canvas.configure(text="File is not an image")
+        except AttributeError:
+            self.canvas.configure(text="File not chosen")
+
+def main():
+    window = Window()
+
+
+if __name__ == "__main__":
+    main()
