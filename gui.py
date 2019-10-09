395a6a3b9d7534f5cbbec38bd0d9bfb87a9eac97
JakPopielarz <popielarzjakub7@gmail.com>
2019-10-09 15:20:44 +0200

    Minor refactor

--- gui.py
+++ gui.py
@@ -183,23 +183,8 @@ class Window():
                                     text="File not chosen")
 
     def teach(self):
-        """
-        Create samples so the algorigthm can learn
-        """
-#Solution for alt-tabbing: pop up a window with text entry field in which you enter
-#the sum of money and then it is saved as responses (without ",") etc etc
+        # collect correct input from user and add it to the knowledge-base
         amount = simpledialog.askstring("Correct amount", 
                                         "Enter correct amount [always with 2 digits after coma]")
         if amount and len(amount) == len(self.image.bounding_rectangles)+1:
             self.image.save_correct_recognition(amount)
-#        for i in range(len(self.image.bounding_rectangles)):
-#            roi = self.image.prepare_sample(i)
-#
-#            self.canvas.delete("photo")
-#            self.set_canvas_photo()
-#            self.root.update()
-#
-#            self.image.gather_answer(i)
-#            self.image.add_sample(roi)
-#
-#        self.image.save_results()
