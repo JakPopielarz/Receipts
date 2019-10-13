27841807d1801bafc5d225ab8a07737342efa2ed
JakPopielarz <popielarzjakub7@gmail.com>
2019-10-13 21:39:43 +0200

    csv writing implementation

--- database.py
+++ database.py
@@ -6,9 +6,29 @@ Created on Thu Oct 10 11:23:32 2019
 @author: jakub
 """
 
+import csv
+from receipt import Receipt
+
 class Database():
-    def __init__(self, receipts=[]):
-        self.receipts = receipts
+    def __init__(self):
+        self.receipts = []
+        new_csv = False
+
+        try:
+            with open("receipts_summation.csv", "r") as csv_file:
+                read_file = csv.reader(csv_file)
+
+                for row in list(read_file)[1:]:
+                    self.receipts.append(Receipt(row[:-1], row[-1]))
+
+        except FileNotFoundError:
+            self.receipts = []
+            new_csv = True
+
+        with open("receipts_summation.csv", "a") as file:
+            csv_file = csv.writer(file)
+            if new_csv:
+                csv_file.writerow(["day", "month", "year", "amount"])
 
     def __str__(self):
         string = ""
@@ -19,3 +39,6 @@ class Database():
 
     def add_receipt(self, bill):
         self.receipts.append(bill)
+        with open("receipts_summation.csv", "a") as file:
+            csv_file = csv.writer(file)
+            csv_file.writerow(bill.to_list())
--- receipt.py
+++ receipt.py
@@ -8,15 +8,30 @@ Created on Thu Oct 10 11:21:47 2019
 
 class Receipt():
     def __init__(self, date, amount):
-        date = date.split(".")
-        self.date = {"day": int(date[0]),
-                     "month": int(date[1]),
-                     "year": int(date[2])}
+        if type(date) == str:
+            date = date.split(".")
+            self.date = {"day": int(date[0]),
+                         "month": int(date[1]),
+                         "year": int(date[2])}
+        elif type(date) == list:
+            self.date = {"day": int(date[0]),
+                         "month": int(date[1]),
+                         "year": int(date[2])}
 
-        self.amount = amount
+        self.amount = float(amount)
 
     def __str__(self):
+
+        return "Date: " + self.get_printable_date() + "; Amount: " + str(self.amount)
+
+    def get_printable_date(self):
         date = self.date.values()
         date = [str(x) for x in date]
 
-        return "Date: " + ".".join(date) + "; Amount: " + str(self.amount)
+        return ".".join(date)
+
+    def to_list(self):
+        arr = list(self.date.values())
+        arr.append(self.amount)
+
+        return arr
